583673f072accc7cba5ed5d65b9fe1878f983877
Fernando Garcia <fergarciadlc@gmail.com>
2025-02-09 17:33:46 +0100

    Process_audio_collection script working!!

--- src/process_audio_collection.py
+++ src/process_audio_collection.py
@@ -1,13 +1,16 @@
-# src/audio_analysis/process_audio_collection.py
-## python process_audio_collection.py
+# cd src
+# python process_audio_collection.py
 import logging
 
-import tqdm
+import essentia
+from tqdm import tqdm
 
 from audio_analysis.config import EXTRACTORS, RAW_DIR
 from audio_analysis.features import AudioFeaturesExtractor
 from audio_analysis.loaders import load_all_audio_files
 from utils.audio import load_audio
+
+essentia.log.warningActive = False
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -21,12 +24,16 @@ def process_audio_collection(root_dir: str):
         audio_data = load_audio(audio_file)
         audio_features = AudioFeaturesExtractor(
             extractors=EXTRACTORS,
-        ).extract()
+        ).extract(audio_data=audio_data)
         collection_features.append(audio_features)
-        logger.info(f"Extracted features for {audio_file}: {audio_features}")
+        # logger.info(f"Extracted features for {audio_file}: {audio_features}")
 
     print(collection_features)
 
 
+def main():
+    process_audio_collection(str(RAW_DIR))
+
+
 if __name__ == "__main__":
-    process_audio_collection(str(RAW_DIR.resolve()))
+    main()
